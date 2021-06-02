d81589a467d15ac34884ba7e52777d137bfbaf84
Jarrell28 <jhouston2882@yahoo.com>
2021-06-02 18:48:22 -0500

    completed lab

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,19 +1,33 @@
 import React from 'react';
 
-import ListGroup from 'react-bootstrap/ListGroup';
+import Badge from 'react-bootstrap/Badge';
+import Modal from 'react-bootstrap/Modal';
 
 function TodoList(props) {
 
   return (
-    <ListGroup as="ul">
-      {props.list.map(item => (
-        <ListGroup.Item as="li" variant={item.complete ? "success" : "danger"}
-          key={item._id} onClick={() => props.handleComplete(item._id)}>
-          {item.text} - {item.assignee}
-        </ListGroup.Item>
-      ))}
-    </ListGroup>
-    // className = {`complete-${item.complete.toString()}`}
+    <div className="d-flex flex-column" style={{
+      minWidth: "300px"
+    }}>
+      {
+        props.list.map(item => (
+          <Modal.Dialog key={item._id} className="m-2 shadow p-0 bg-body rounded">
+            <Modal.Header className="align-items-center">
+              <div>
+                <Badge pill variant={item.complete ? "success" : "danger"} className="mr-3" onClick={() => props.handleComplete(item._id)}>{item.complete ? "pending" : "completed"}</Badge> <span>{item.assignee}</span>
+              </div>
+              <button type="button" className="close" onClick={() => props.handleDelete(item._id)}><span aria-hidden="true">×</span><span className="sr-only">Close</span></button>
+            </Modal.Header>
+            <Modal.Body className="pb-1">
+              {item.text}
+            </Modal.Body>
+            <Modal.Footer className="m-0 border-top-0 pt-0">
+              <p className="m-0" style={{ fontSize: "12px" }}>Difficulty: {item.difficulty}</p>
+            </Modal.Footer>
+          </Modal.Dialog>
+        ))
+      }
+    </div >
   );
 }
 
